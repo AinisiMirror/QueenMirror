a9e3e8101f401b9d9644a2311d402e3bf51ec8a2
Administrator <2931748796@qq.com>
2018-04-10 10:38:58 +0800

    修改订单UI页面

--- app/src/main/java/com/ainisi/queenmirror/queenmirrorcduan/ui/home/activity/MineMyFootActivity.java
+++ app/src/main/java/com/ainisi/queenmirror/queenmirrorcduan/ui/home/activity/MineMyFootActivity.java
@@ -19,17 +19,22 @@ import java.util.List;
 
 import butterknife.Bind;
 import butterknife.OnClick;
+
 //我的足迹
-public class  MineMyFootActivity extends BaseActivity{
+public class MineMyFootActivity extends BaseActivity {
 
     @Bind(R.id.mine_foot_recycler)
     RecyclerView recycler;
     @Bind(R.id.title_title)
     TextView folltitle;
-    List<SortBean> sortlist=new ArrayList<>();
+    @Bind(R.id.title_right)
+    TextView titleRight;
+    List<SortBean> sortlist = new ArrayList<>();
+
     public static void startActivity(Context context) {
         context.startActivity(new Intent(context, MineMyFootActivity.class));
     }
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_mine_my_foot;
@@ -43,9 +48,10 @@ public class  MineMyFootActivity extends BaseActivity{
     @Override
     public void initView() {
         folltitle.setText("我的足迹");
+        titleRight.setText("编辑");
 
-        for (int i = 0; i <10 ; i++) {
-            SortBean sortBean=new SortBean();
+        for (int i = 0; i < 10; i++) {
+            SortBean sortBean = new SortBean();
             sortBean.setName("MOCO美容美发沙龙");
             sortBean.setTime("营业时间 9:00-20:00");
             sortBean.setLogo(R.drawable.ic_sortrecyle_logo);
@@ -54,11 +60,12 @@ public class  MineMyFootActivity extends BaseActivity{
             sortlist.add(sortBean);
         }
 
-        MyAdapter sortAdapter=new MyAdapter(this,sortlist,R.layout.item_fullshortrecycler);
-        recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        MyAdapter sortAdapter = new MyAdapter(this, sortlist, R.layout.item_fullshortrecycler);
+        recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         recycler.setAdapter(sortAdapter);
 
     }
+
     @OnClick({R.id.title_back
     })
     public void click(View view) {
