45bbb79b5b6a47f35ffafbe6c282442b31e3a39c
Administrator <2931748796@qq.com>
2018-04-20 09:55:44 +0800

    重新分类

---
+++ app/src/main/java/com/ainisi/queenmirror/queenmirrorcduan/ui/mine/activity/HerDynamicActivity.java
@@ -0,0 +1,34 @@
+package com.ainisi.queenmirror.queenmirrorcduan.ui.mine.activity;
+
+import android.widget.TextView;
+
+import com.ainisi.queenmirror.common.base.BaseActivity;
+import com.ainisi.queenmirror.queenmirrorcduan.R;
+
+import butterknife.Bind;
+
+/**
+ * 动态（是否让看）
+ */
+public class HerDynamicActivity extends BaseActivity
+{
+    @Bind(R.id.newtitle_title)
+    TextView hertitle;
+    private String title;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_her_dynamic;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void initView() {
+        title =getIntent().getStringExtra("dynamic");
+        hertitle.setText(title);
+    }
+}
